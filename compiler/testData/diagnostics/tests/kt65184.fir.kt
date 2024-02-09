b3c7394024f9d0bc7a11afa9838485c1bdc4a329
vladislav.grechko <vladislav.grechko@jetbrains.com>
2024-02-09 17:16:52 +0100

    [FIR] Refine `ConeKotlinType` substitution rules
     1. When looking for supertypes, do not substitute nullable type to
     into the definitely non-nullable type parameter
    
     2. When substituting to `ConeFlexibleType`, choose one of the bounds
     when failed to substitute into the another
    
     ^KT-65184: Fixed

---
+++ compiler/testData/diagnostics/tests/kt65184.fir.kt
@@ -0,0 +1,24 @@
+// FULL_JDK
+
+// FILE: A.java
+
+public class A<T> {
+
+}
+
+// FILE: B.java
+
+public class B<T> extends A<T> {
+
+}
+
+// FILE: box.kt
+
+import java.util.LinkedList
+import java.util.Queue
+
+fun bar(b: A<String>) {}
+
+fun test() {
+    bar(<!ARGUMENT_TYPE_MISMATCH!>B<String?>()<!>)
+}
