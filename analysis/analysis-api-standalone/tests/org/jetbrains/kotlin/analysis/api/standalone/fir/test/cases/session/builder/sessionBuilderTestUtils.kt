8e713584100e0e8c50ba873155d1cd5cd9a75340
Jinseong Jeon <jsjeon@google.com>
2024-02-13 10:28:07 -0800

    AA: add support VirtualFile inputs to source module
    ^KT-65571 fixed

--- analysis/analysis-api-standalone/tests/org/jetbrains/kotlin/analysis/api/standalone/fir/test/cases/session/builder/sessionBuilderTestUtils.kt
+++ analysis/analysis-api-standalone/tests/org/jetbrains/kotlin/analysis/api/standalone/fir/test/cases/session/builder/sessionBuilderTestUtils.kt
@@ -5,6 +5,10 @@
 
 package org.jetbrains.kotlin.analysis.api.standalone.fir.test.cases.session.builder
 
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.VirtualFileListener
+import com.intellij.openapi.vfs.VirtualFileSystem
 import org.jetbrains.kotlin.analysis.api.analyze
 import org.jetbrains.kotlin.analysis.api.calls.KtSuccessCallInfo
 import org.jetbrains.kotlin.analysis.api.calls.successfulFunctionCallOrNull
@@ -12,6 +16,7 @@ import org.jetbrains.kotlin.analysis.api.calls.symbol
 import org.jetbrains.kotlin.analysis.api.symbols.KtFunctionSymbol
 import org.jetbrains.kotlin.name.CallableId
 import org.jetbrains.kotlin.psi.KtCallExpression
+import org.jetbrains.kotlin.psi.KtPsiFactory
 import org.jetbrains.kotlin.test.MockLibraryUtil
 import org.jetbrains.kotlin.test.util.KtTestUtil
 import java.nio.file.Files
@@ -21,6 +26,10 @@ import kotlin.io.path.absolutePathString
 import kotlin.io.path.extension
 import kotlin.streams.asSequence
 import org.junit.jupiter.api.Assertions
+import java.io.InputStream
+import java.io.OutputStream
+import java.time.LocalDateTime
+import java.time.ZoneOffset
 
 internal fun testDataPath(path: String): Path {
     return Paths.get("analysis/analysis-api-standalone/testData/sessionBuilder").resolve(path)
@@ -36,7 +45,6 @@ fun KtCallExpression.assertIsCallOf(callableId: CallableId) {
     }
 }
 
-
 internal fun compileCommonKlib(kLibSourcesRoot: Path): Path {
     val ktFiles = Files.walk(kLibSourcesRoot).asSequence().filter { it.extension == "kt" }.toList()
     val testKlib = KtTestUtil.tmpDir("testLibrary").resolve("library.klib").toPath()
@@ -49,4 +57,108 @@ internal fun compileCommonKlib(kLibSourcesRoot: Path): Path {
     MockLibraryUtil.runMetadataCompiler(arguments)
 
     return testKlib
+}
+
+internal fun createVirtualFileOnTheFly(
+    project: Project,
+    text: String,
+    fs: VirtualFileSystem = TestVirtualFileSystem(),
+): VirtualFile {
+    val factory = KtPsiFactory(project, markGenerated = false)
+    val ktFile = factory.createFile(text)
+    return object : VirtualFile() {
+        override fun getName(): String = ktFile.name
+
+        override fun getFileSystem(): VirtualFileSystem = fs
+
+        override fun getPath(): String {
+            TODO("Not yet implemented")
+        }
+
+        override fun isWritable(): Boolean = false
+
+        override fun isDirectory(): Boolean = false
+
+        override fun isValid(): Boolean = ktFile.isValid
+
+        override fun getParent(): VirtualFile {
+            TODO("Not yet implemented")
+        }
+
+        override fun getChildren(): Array<VirtualFile> = emptyArray()
+
+        override fun getOutputStream(requestor: Any?, newModificationStamp: Long, newTimeStamp: Long): OutputStream {
+            TODO("Not yet implemented")
+        }
+
+        override fun contentsToByteArray(): ByteArray = ktFile.text.toByteArray()
+
+        private val timeStamp = LocalDateTime.now().toEpochSecond(ZoneOffset.UTC)
+
+        override fun getTimeStamp(): Long = timeStamp
+
+        override fun getLength(): Long = ktFile.textLength.toLong()
+
+        override fun refresh(asynchronous: Boolean, recursive: Boolean, postRunnable: Runnable?) {
+            TODO("Not yet implemented")
+        }
+
+        override fun getInputStream(): InputStream {
+            TODO("Not yet implemented")
+        }
+    }
+}
+
+private class TestVirtualFileSystem : VirtualFileSystem() {
+    override fun getProtocol(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun findFileByPath(path: String): VirtualFile? {
+        TODO("Not yet implemented")
+    }
+
+    override fun refresh(asynchronous: Boolean) {
+        TODO("Not yet implemented")
+    }
+
+    override fun refreshAndFindFileByPath(path: String): VirtualFile? {
+        TODO("Not yet implemented")
+    }
+
+    override fun addVirtualFileListener(listener: VirtualFileListener) {
+        TODO("Not yet implemented")
+    }
+
+    override fun removeVirtualFileListener(listener: VirtualFileListener) {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteFile(requestor: Any?, vFile: VirtualFile) {
+        TODO("Not yet implemented")
+    }
+
+    override fun moveFile(requestor: Any?, vFile: VirtualFile, newParent: VirtualFile) {
+        TODO("Not yet implemented")
+    }
+
+    override fun renameFile(requestor: Any?, vFile: VirtualFile, newName: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun createChildFile(requestor: Any?, vDir: VirtualFile, fileName: String): VirtualFile {
+        TODO("Not yet implemented")
+    }
+
+    override fun createChildDirectory(requestor: Any?, vDir: VirtualFile, dirName: String): VirtualFile {
+        TODO("Not yet implemented")
+    }
+
+    override fun copyFile(requestor: Any?, virtualFile: VirtualFile, newParent: VirtualFile, copyName: String): VirtualFile {
+        TODO("Not yet implemented")
+    }
+
+    override fun isReadOnly(): Boolean {
+        TODO("Not yet implemented")
+    }
 }
