7c1593ae4200622e7b4e0263bf27ee7ab4af1101
Anton Lakotka <anton.lakotka@jetbrains.com>
2024-02-14 11:27:43 +0100

    [Gradle] Associate Android java sources with related Kotlin Source Set
    Java sources can be produced by the task or can be configured with
    task dependencies. Previously sources were included into task directly
    now they are properly configured with Kotlin Android Source Set.
    
    ^KT-63753 Verification Pending
    ^KT-65426 Verification Pending

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/android/AndroidProjectHandler.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/android/AndroidProjectHandler.kt
@@ -229,15 +229,11 @@ internal class AndroidProjectHandler(
         }
 
         val javaTask = variantData.javaCompileProvider
-        @Suppress("UNCHECKED_CAST") val kotlinTask = compilation.compileTaskProvider as TaskProvider<KotlinCompile>
-        compilation.androidVariant.forEachJavaSourceDir { sources ->
-            // It is important to pass exactly `sources.dir` as provider with explicit task dependency
-            // Because of the following bugs:
-            // * https://github.com/gradle/gradle/issues/27881 ConfigurableFileTree.from() doesn't preserve Task Dependencies
-            // * https://github.com/gradle/gradle/issues/27882 SourceDirectorySet doesn't accept ConfigurableFileTree
-            val sourceDirWithTaskDependencies = project.filesProvider(sources) { sources.dir }
-            compilation.defaultSourceSet.kotlin.srcDir(sourceDirWithTaskDependencies)
+
+        variantData.forEachJavaSourceDir { sources ->
+            compilation.defaultSourceSet.kotlin.srcDir(project.fileCollectionFromConfigurableFileTree(sources))
         }
+        @Suppress("UNCHECKED_CAST") val kotlinTask = compilation.compileTaskProvider as TaskProvider<KotlinCompile>
         wireKotlinTasks(project, compilation, androidPlugin, androidExt, variantData, javaTask, kotlinTask)
     }
 
