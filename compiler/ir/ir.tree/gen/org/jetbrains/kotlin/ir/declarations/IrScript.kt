a7ddfd17612d70a1868fb75be68be85e3be85502
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2024-02-16 12:47:08 +0100

    [IR] Autogenerate IrScriptImpl
    ^KT-65773 In Progress

--- compiler/ir/ir.tree/gen/org/jetbrains/kotlin/ir/declarations/IrScript.kt
+++ compiler/ir/ir.tree/gen/org/jetbrains/kotlin/ir/declarations/IrScript.kt
@@ -8,6 +8,8 @@
 
 package org.jetbrains.kotlin.ir.declarations
 
+import org.jetbrains.kotlin.descriptors.ScriptDescriptor
+import org.jetbrains.kotlin.ir.ObsoleteDescriptorBasedAPI
 import org.jetbrains.kotlin.ir.expressions.IrStatementContainer
 import org.jetbrains.kotlin.ir.symbols.IrClassSymbol
 import org.jetbrains.kotlin.ir.symbols.IrPropertySymbol
@@ -25,6 +27,9 @@ import org.jetbrains.kotlin.ir.visitors.IrElementVisitor
  */
 abstract class IrScript : IrDeclarationBase(), IrDeclarationWithName, IrDeclarationParent, IrStatementContainer, IrMetadataSourceOwner {
     abstract override val symbol: IrScriptSymbol
+
+    @ObsoleteDescriptorBasedAPI
+    abstract override val descriptor: ScriptDescriptor
 
     abstract var thisReceiver: IrValueParameter?
 
