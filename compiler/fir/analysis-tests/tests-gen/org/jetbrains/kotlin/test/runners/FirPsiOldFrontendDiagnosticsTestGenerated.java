5fd28762e2f7ef190bdbff4436f1023806b7692f
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2024-02-16 09:15:59 +0100

    fixup! [Tests] Add box test for #KT-65555

--- compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirPsiOldFrontendDiagnosticsTestGenerated.java
+++ compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirPsiOldFrontendDiagnosticsTestGenerated.java
@@ -26910,12 +26910,6 @@ public class FirPsiOldFrontendDiagnosticsTestGenerated extends AbstractFirPsiDia
                 }
 
                 @Test
-                @TestMetadata("kt65555.kt")
-                public void testKt65555() throws Exception {
-                    runTest("compiler/testData/diagnostics/tests/override/clashesOnInheritance/kt65555.kt");
-                }
-
-                @Test
                 @TestMetadata("kt9550.kt")
                 public void testKt9550() throws Exception {
                     runTest("compiler/testData/diagnostics/tests/override/clashesOnInheritance/kt9550.kt");
