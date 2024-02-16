36ee117c59458dc91041477ac818c1235905ed13
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2024-02-16 12:45:31 +0100

    [IR] Autogenerate IrValueParameterImpl
    ^KT-65773 In Progress

--- compiler/ir/ir.tree/tree-generator/src/org/jetbrains/kotlin/ir/generator/ImplementationConfigurator.kt
+++ compiler/ir/ir.tree/tree-generator/src/org/jetbrains/kotlin/ir/generator/ImplementationConfigurator.kt
@@ -88,7 +88,7 @@ object ImplementationConfigurator : AbstractIrTreeImplementationConfigurator() {
         }
 
         impl(valueParameter) {
-            implementation.doPrint = false
+            defaultNull("defaultValue")
         }
 
         impl(variable) {
